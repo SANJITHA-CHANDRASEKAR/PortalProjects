bdf088e7c67ae6d86ba19ae976999fc43cb691e5
SANJITHA-CHANDRASEKAR <sanjitha2903@gmail.com>
2023-02-21 14:14:15 +0530

    portalprojects

---
+++ src/main/java/com/example/demo/color.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class color {
+		private String studentName="Sanju";
+		@GetMapping("/")
+		public String getName() {
+			return "Welcome "+studentName;
+		}
+}
